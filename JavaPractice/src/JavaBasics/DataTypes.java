09790701444cbe9631bd514ba7f8a65351e559f1
Raju <Raju@rajus-mbp.attlocal.net>
2019-04-04 00:36:28 -0400

    First commit

---
+++ JavaPractice/src/JavaBasics/DataTypes.java
@@ -0,0 +1,47 @@
+package JavaBasics;
+
+public class DataTypes {
+	
+	public static void main(String[] args) {
+		//primitive data type = int, double, char , boolean
+		
+		//1. int:
+		int i =10;
+		i =20; //the value of i which was 10 will be change to 20.
+		int j = 50;
+		System.out.println(i);
+		int k =-1;
+		int l = 0;
+		
+		//2. double:
+		double d = 12.33;
+		double d1 = 34.44;
+		
+		System.out.println(d+d1);
+		
+		double d2 = 100; //100.00
+		
+		//3. char: only single digit value.
+		char c = 'a'; //char should be written inside single codes.
+		char c1 ='A';
+		char c2 = '1';
+		char c3  = 's';
+		
+		//4. boolenan:
+		boolean b1 = true;
+		boolean b2 = false;
+		
+		//true & Value are keyword or value: these are value but it is also keyword
+		
+		//5. String; is a class not a data type but it can be use as data tpe
+		String s = "Hello World";
+		String s1 = "Selenium";
+		String s2 = "Hi this is my java code";
+		String s3 = "1000";
+		String s4 = "12.33";
+		
+		System.out.println(s2);
+		
+	}
+
+}
